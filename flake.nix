90328b89ea8e5ca644ac680cfe0c494720598c71
Louis Pearson <opensource@louispearson.work>
2023-03-09 15:13:17 -0700

    Android stuff

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
     zig-overlay.url = "github:mitchellh/zig-overlay";
+    android.url = "github:tadfisher/android-nixpkgs";
 
     zig-overlay.inputs = {
       nixpkgs.follows = "nixpkgs";
@@ -10,17 +12,38 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig-overlay }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, android, devshell }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk zig;
+      };
+    }
+    //
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+          config.allowUnfree = true;
           overlays = [
-            (zig-overlay.overlays.default)
+            devshell.overlay
+            self.overlay
           ];
         };
       in
       rec {
+        packages = {
+          zig = zig-overlay.packages.${system}.master;
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            build-tools-33-0-0
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-21
+            ndk-25-1-8937393
+          ]);
+        };
+
+        devShells.android = import ./devshell.nix { inherit pkgs; };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             pkg-config
