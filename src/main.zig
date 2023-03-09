6f8c9080d08f6b9227c61aa655f48f6373a60d19
Louis Pearson <opensource@louispearson.work>
2023-03-09 15:56:51 -0700

    Learn to use FormatDataWrapper

--- src/main.zig
+++ src/main.zig
@@ -4,31 +4,38 @@ const capy = @import("capy");
 // This is required for your app to build to WebAssembly and other particular architectures
 pub usingnamespace capy.cross_platform;
 
-// const TestStruct = struct {
-//     int: capy.DataWrapper(u32) = capy.DataWrapper(u32).of(0),
-// };
+const TestStruct = struct {
+    int: capy.DataWrapper(u32) = capy.DataWrapper(u32).of(0),
+};
 var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 var arena = std.heap.ArenaAllocator.init(general_purpose_allocator.allocator());
+const lasting_allocator = general_purpose_allocator.allocator();
 const allocator = arena.allocator();
+
+var test_struct = TestStruct{};
 
 pub fn main() !void {
     defer arena.deinit();
     try capy.backend.init();
 
-    // var test_struct = TestStruct{};
+    var hello_text = try capy.FormatDataWrapper(lasting_allocator, "You clicked save {} times", .{&test_struct.int});
 
     var window = try capy.Window.init();
-    try window.set(
-        capy.Column(.{ .spacing = 10 }, .{ // have 10px spacing between each column's element
-            capy.Row(.{ .spacing = 5 }, .{ // have 5px spacing between each row's element
-                capy.Button(.{ .label = "Save", .onclick = @ptrCast(*const fn (*anyopaque) anyerror!void, &buttonClicked) }),
-                capy.Button(.{ .label = "Run", .onclick = buttonClicked2 }),
-            }),
-            // Expanded means the widget will take all the space it can
-            // in the parent container
-            capy.Expanded(capy.Label(.{ .text = "Hello World!" })),
-        }),
-    );
+    try window.set(capy.Margin(
+        capy.Rectangle.init(10, 10, 10, 10),
+        capy.Column(
+            .{ .spacing = 10 },
+            .{ // have 10px spacing between each column's element
+                capy.Row(.{ .spacing = 5 }, .{ // have 5px spacing between each row's element
+                    capy.Button(.{ .label = "Save", .onclick = @ptrCast(*const fn (*anyopaque) anyerror!void, &buttonClicked) }),
+                    capy.Button(.{ .label = "Run", .onclick = buttonClicked2 }),
+                }),
+                // Expanded means the widget will take all the space it can
+                // in the parent container
+                capy.Expanded(capy.Label(.{}).bind("text", hello_text)),
+            },
+        ),
+    ));
 
     window.resize(800, 600);
     window.show();
@@ -40,8 +47,7 @@ pub fn main() !void {
 
 fn buttonClicked(button: *capy.Button_Impl) anyerror!void {
     std.log.info("You clicked button with text {s}", .{button.getLabel()});
-    // const new_text = try std.fmt.allocPrintZ(allocator, "You clicked button with text {s}", .{button.getLabel()});
-    // button.setLabel(new_text);
+    test_struct.int.set(test_struct.int.get() + 1);
 }
 
 fn buttonClicked2(_: *anyopaque) anyerror!void {
